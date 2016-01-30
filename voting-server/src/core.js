3896302dff0a3e1a443cf3cdcc54a6004a3cc0f1
Jonathan Frederic <jon.freder@gmail.com>
2016-01-30 10:21:08 -0800

    10:21am

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -3,7 +3,35 @@ export function setEntries(state, entries) {
     return state.set('entries', fromJS(entries));
 }
 
+function getWinners(state) {
+    const voteState = state.get('vote');
+    if (!voteState) return [];
+    
+    const [a, b] = voteState.get('pair');
+    const aVotes = voteState.getIn(['tally', a], 0);
+    const bVotes = voteState.getIn(['tally', b], 0);
+    if (aVotes>bVotes) {
+        return [a];
+    } else if (aVotes<bVotes) {
+        return [b];
+    } else {
+        return [a, b];
+    }
+}
+
 export function next(state) {
-    let nextState = state.set('vote', Map({pair: state.get('entries').slice(0,2)}));
-    return nextState.update('entries', entries => entries.slice(2));
+    // let nextState = state.set('vote', Map({pair: state.get('entries').slice(0,2)}));
+    // return nextState.update('entries', entries => entries.slice(2));
+    const entries = state.get('entries');
+    return state.merge(fromJS({
+        vote: {pair: entries.take(2)},
+        entries: entries.skip(2).concat(getWinners(state))
+    }));
+}
+
+export function vote(state, value) {
+    // return state.mergeDeep(fromJS({
+    //     vote: {tally: (Map()).set(value, state.get('vote').get('tally', Map()).get(value, 0) + 1)},
+    // }));
+    return state.updateIn(['vote', 'tally', value], 0, tally => tally + 1);
 }
--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map, fromJS} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', ()=>{
     describe('setEntries', ()=>{
@@ -32,6 +32,72 @@ describe('application logic', ()=>{
                     pair: ['a', 'b']
                 },
                 entries: ['c']
+            }));
+        });
+        it('puts winner of current vote back into entries', ()=>{
+            const state = fromJS({
+                vote: {
+                    pair: ['d', 'e'],
+                    tally: {
+                        d: 2,
+                        e: 4
+                    }
+                },
+                entries: ['a', 'b', 'c']
+            });
+            const nextState = next(state);
+            
+            expect(nextState).to.equal(fromJS({
+                vote: {
+                    pair: ['a', 'b']
+                },
+                entries: ['c', 'e']
+            }));
+        });
+    });
+    
+    describe('vote', ()=>{
+        it('creates a tally for the voted entry', ()=>{
+            const state = fromJS({
+                vote: {
+                    pair: ['a', 'b']
+                },
+                entries: []
+            });
+            const nextState = vote(state, 'a');
+            
+            expect(nextState).to.equal(fromJS({
+                vote: {
+                    pair: ['a', 'b'],
+                    tally: {
+                        'a': 1
+                    }
+                },
+                entries: []
+            }));
+        });
+        it('adds a tally for the voted entry', ()=>{
+            const state = fromJS({
+                vote: {
+                    pair: ['a', 'b'],
+                    tally: {
+                        'a': 2,
+                        'b': 3
+                    }
+                },
+                entries: []
+            });
+            const nextState = vote(state, 'b');
+            
+            expect(nextState).to.equal(fromJS({
+                vote: {
+                    pair: ['a', 'b'],
+                    tally: {
+                        'a': 2,
+                        'b': 4
+                    }
+                },
+                entries: []
             }));
         });
     });

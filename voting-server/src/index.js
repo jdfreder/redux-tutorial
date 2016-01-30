ff0e9ff2769f59ac3a4ba2126847048860980a3e
Jonathan Frederic <jon.freder@gmail.com>
2016-01-30 11:32:39 -0800

    11:32am

--- voting-server/src/index.js
+++ voting-server/src/index.js
@@ -1,3 +1,11 @@
 import makeStore from './src/store';
+import startServer from './server';
 
 export const store = makeStore();
+startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
